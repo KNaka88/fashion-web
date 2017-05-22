4ddc28065d02d7a7b7a529be6e5c796ad1550bbb
Koji Nakagawa <lightupthesky0627@gmail.com>
2017-05-21 18:16:09 -0700

    add initial files and npm (material design lite)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+import {MdButtonModule, MdCheckboxModule} from '@angular/material';
+import { IndexComponent } from './index/index.component';
+import { routing } from './app.routing';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    BrowserAnimationsModule,
+    MdButtonModule, MdCheckboxModule,
+    routing,
   ],
   providers: [],
   bootstrap: [AppComponent]
